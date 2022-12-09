1e3738d6f00afb79e9ea2820d3be4b8224bc25c3
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2022-12-09 11:03:16 +0400

    jquey

---
+++ 25. jquery/main.js
@@ -0,0 +1,58 @@
+// $(document).ready(() => {
+//   $(".test h1").click(function () {
+//     // $(this).hide();
+//     this.style.display = "none";
+//   });
+// });
+
+// const heading = document.getElementsByTagName("h1");
+// console.log("getElementsByTagName", heading);
+
+// const h = document.querySelectorAll("h1");
+// console.log("querySelectorAll", h);
+
+// const arr = Array.from(heading);
+
+// arr.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     e.style.display = "none";
+//   });
+// });
+
+// Array.from(heading, (e) => {
+//   e.addEventListener("click", () => {
+//     e.style.display = "none";
+//   });
+// });
+
+// $(document).ready(()=>{
+//     $("h1").mouseenter(function(){
+//         $(this).hide();
+//      })
+// })
+
+// $(document).ready(function () {
+//   $("#hide").click(function () {
+//     // $("p").hide();
+//     $("p").css("visibility", "hidden");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#show").click(function () {
+//     $("p").style.display="none"
+//     // $("p").css("visibility", "visible");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#change").click(function () {
+//     $("p").css("background-color", "yellow", 1000);
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#change").click(function () {
+//     $("p").animate({ marginLeft: "250px" });
+//   });
+// });
---
+++ 25. number-prototype.js
@@ -0,0 +1,22 @@
+Number.prototype.add = function (num) {
+  num = this + num;
+  return num;
+};
+
+Number.prototype.subtract = function (num) {
+  num = this - num;
+  return num;
+};
+
+Number.prototype.divide = function (num) {
+  num = this / num;
+  return num;
+};
+
+Number.prototype.multiply = function (num) {
+  num = this * num;
+  return num;
+};
+
+const res = (6).add(5).subtract(7).divide(5).multiply(3);
+console.log(res);
